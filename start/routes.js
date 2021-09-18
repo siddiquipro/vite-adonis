f1d410186490a23e7f6865d3587913d4999f4b27
M A Siddiqui <masiddiqui91@gmail.com>
2021-09-18 16:21:36 +0100

    put the values in env

--- start/routes.js
+++ start/routes.js
@@ -4,25 +4,23 @@ const Route = use("Route");
 const Env = use("Env");
 const Helpers = use("Helpers");
 
-const isProd = Env.get("NODE_ENV", process.env.NODE_ENV) != "development" ? true : false;
-
-let data = {
-	script: `<script type="module" src="http://localhost:3000/@vite/client"></script> <script type="module" src="http://localhost:3000/www/main.js"></script>`,
-	css: false,
-};
-
-try {
-	if (isProd) {
-		const constants = use(Helpers.appRoot("constants"));
-		const manifest = use(Helpers.publicPath("build/manifest.json"));
-		data.manifest = manifest;
-		data.script = `<script type="module" src="/build/${manifest[constants.ENTRY]["file"]}"></script>`;
-		if (manifest[constants.ENTRY]["css"][0]) data.css = `<link rel="stylesheet" href="/build/${manifest[constants.ENTRY]["css"][0]}">`;
-	}
-} catch (error) {
-	console.log("Prod manifest update failed", error);
-}
-
 Route.any("*", ({ view }) => {
+	const isProd = Env.get("NODE_ENV", process.env.NODE_ENV) == "development" ? true : false;
+	let data = {
+		script: `<script type="module" src="http://localhost:3000/@vite/client"></script> <script type="module" src="http://localhost:3000/www/main.js"></script>`,
+		css: false,
+	};
+
+	try {
+		if (isProd) {
+			const constants = use(Helpers.appRoot("constants"));
+			const manifest = use(Helpers.publicPath("build/manifest.json"));
+			data.manifest = manifest;
+			data.script = `<script type="module" src="/build/${manifest[constants.ENTRY]["file"]}"></script>`;
+			if (manifest[constants.ENTRY]["css"][0]) data.css = `<link rel="stylesheet" href="/build/${manifest[constants.ENTRY]["css"][0]}">`;
+		}
+	} catch (error) {
+		console.log("Prod manifest update failed", error);
+	}
 	return view.render("welcome", data);
 });
