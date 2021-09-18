540953505ff727dc3c7ef62d59bbd60982bd4b17
M A Siddiqui <masiddiqui91@gmail.com>
2021-09-18 16:09:47 +0100

    log error for prod file

--- start/routes.js
+++ start/routes.js
@@ -4,18 +4,22 @@ const Route = use("Route");
 const Env = use("Env");
 const Helpers = use("Helpers");
 const isProd = Env.get("NODE_ENV") != "development" ? true : false;
-const constants = use(Helpers.appRoot("constants"));
 
 let data = {
 	script: `<script type="module" src="http://localhost:3000/@vite/client"></script> <script type="module" src="http://localhost:3000/www/main.js"></script>`,
 	css: false,
 };
 
-if (isProd) {
-	const manifest = use(Helpers.publicPath("build/manifest.json"));
-	data.manifest = manifest;
-	data.script = `<script type="module" src="/build/${manifest[constants.ENTRY]["file"]}"></script>`;
-	if (manifest[constants.ENTRY]["css"][0]) data.css = `<link rel="stylesheet" href="/build/${manifest[constants.ENTRY]["css"][0]}">`;
+try {
+	if (isProd) {
+		const constants = use(Helpers.appRoot("constants"));
+		const manifest = use(Helpers.publicPath("build/manifest.json"));
+		data.manifest = manifest;
+		data.script = `<script type="module" src="/build/${manifest[constants.ENTRY]["file"]}"></script>`;
+		if (manifest[constants.ENTRY]["css"][0]) data.css = `<link rel="stylesheet" href="/build/${manifest[constants.ENTRY]["css"][0]}">`;
+	}
+} catch (error) {
+	console.log("Prod manifest update failed", error);
 }
 
 Route.any("*", ({ view }) => {
